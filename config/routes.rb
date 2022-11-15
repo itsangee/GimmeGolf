3dfd42d5eb6effdc0102b31c59a3a6f8917e13c0
angelee otic <otic.angeleemae@gmail.com>
2022-11-15 21:19:47 +0000

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
-  root to: "pages#home"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root to: "index#home"
 
+  resources :courses, [:index, :show] do
+    resources :bookings, [:index, :create, :new]
+  end
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :bookings, [:show, :edit, :update, :delete]
+
+  resources :attendees, [:create, :delete]
   # Defines the root path route ("/")
   # root "articles#index"
 end
