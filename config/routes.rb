f511687d5c8f75c7845adfed724db6d95be8ab4d
angelee otic <otic.angeleemae@gmail.com>
2022-11-17 19:26:47 +0000

    fixed routes and cloudinary

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,13 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "index#home"
 
-  resources :courses, [:index, :show] do
-    resources :bookings, [:index, :create, :new]
+  resources :courses, only: [:index, :show] do
+    resources :bookings, only: [:index, :create, :new]
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :bookings, [:show, :edit, :update, :delete]
+  resources :bookings, only: [:show, :edit, :update, :delete]
 
-  resources :attendees, [:create, :delete]
+  resources :attendees, only: [:create, :delete]
   # Defines the root path route ("/")
   # root "articles#index"
 end
