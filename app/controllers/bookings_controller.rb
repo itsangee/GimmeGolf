3ab41312a6c29e460174d10f130fa0db4a541c13
Mustafa Tahir <mustafatahir@hotmail.co.uk>
2022-11-19 15:01:32 +0000

    datetime added to booking form

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -9,7 +9,7 @@ class BookingsController < ApplicationController
   end
 
   def create
-    @booking = Booking.new
+    @booking = Booking.new(booking_params)
     @course = Course.find(params[:course_id])
     # @booking = Booking.new(booking_params)
     @booking.course = @course
@@ -20,7 +20,8 @@ class BookingsController < ApplicationController
   end
 
   private
+
   def booking_params
-    params.require(:booking).permit(:course_id, :user_id)
+    params.require(:booking).permit(:datetime)
   end
 end
