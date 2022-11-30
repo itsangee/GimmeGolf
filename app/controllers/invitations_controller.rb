9ea6afcef61728716bb043055d01e9df07eddb01
Mustafa Tahir <mustafatahir@hotmail.co.uk>
2022-11-30 23:37:07 +0000

    some changes to booking confirmation

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -2,7 +2,7 @@ class InvitationsController < ApplicationController
 
   def invite_user
     @booking = Booking.find(params[:id])
-     @user = User.find(params[:user])
+    @user = User.find(params[:user])
     @invitation = Invitation.new(booking: @booking, user: @user)
     @invitation.save!
   end
