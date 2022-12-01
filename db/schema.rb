50ba3a3c2c9bd4d51c35044001755da84eae2d0d
magdalena krasicka <krasickamagdalena@gmail.com>
2022-12-01 21:02:32 +0000

    styling has to be finished

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_30_083308) do
-
+ActiveRecord::Schema[7.0].define(version: 2022_11_29_201744) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -123,7 +122,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_30_083308) do
     t.datetime "updated_at", null: false
     t.string "first_name"
     t.string "last_name"
-    t.text "content"
     t.text "bio"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
