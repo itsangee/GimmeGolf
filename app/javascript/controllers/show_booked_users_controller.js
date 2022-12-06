a13aebcc7b95c79930d5d350162f84203bdd8d2d
Mustafa Tahir <mustafatahir@hotmail.co.uk>
2022-12-06 19:22:42 +0000

    javascript for bookings

--- app/javascript/controllers/show_booked_users_controller.js
+++ app/javascript/controllers/show_booked_users_controller.js
@@ -2,12 +2,22 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="show-booked-users"
 export default class extends Controller {
-  static targets = ["users"]
+  static targets = ["users", "slots"]
   connect() {
     console.log('hi');
   }
 
-  showUsers(){
-    console.log("working tbh");
+  showUsers(e){
+
+    const slotId = e.currentTarget.id
+    this.usersTargets.forEach((item) => {
+      if (item.classList.contains(slotId)) {
+        // console.log(`we're in the if statement`)
+        item.classList.remove("d-none")
+      } else {
+        item.classList.add("d-none")
+      }
+    })
+
   }
 }
