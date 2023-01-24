964d090949f19c3856363431f326019144735355
Mustafa Tahir <mustafatahir@hotmail.co.uk>
2023-01-24 22:40:24 +0000

    fixed whitelist

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,7 +1,7 @@
 require 'pry'
 
 class CoursesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:index, :show]
+  skip_before_action :authenticate_user!, only: [:home, :index, :show]
 
   def home
     @courses = Course.all
